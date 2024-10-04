16d95b7229ba90dd8a7ce355544eed30adfb7fa9
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-03 22:14:31 -0300

    criando classe ticket

--- app/src/main/java/br/com/brecori/checkpoint5kotlin/MainActivity.kt
+++ app/src/main/java/br/com/brecori/checkpoint5kotlin/MainActivity.kt
@@ -2,19 +2,22 @@ package br.com.brecori.checkpoint5kotlin
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import br.com.brecori.checkpoint5kotlin.ui.theme.Checkpoint5KotlinTheme
+import androidx.appcompat.widget.Toolbar
+import carreiras.com.github.cryptomonitor.R
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
+        // Configurando a toolbar
+        val toolbarMain: Toolbar = findViewById(R.id.toolbar_main)
+        configureToolbar(toolbarMain)
+    }
+    private fun configureToolbar(toolbar: Toolbar) {
+        setSupportActionBar(toolbar)
+        toolbar.setTitleTextColor(getColor(R.color.white))
+        supportActionBar?.setTitle(getText(R.string.app_title))
+        supportActionBar?.setBackgroundDrawable(getDrawable(R.color.primary))
     }
 }
